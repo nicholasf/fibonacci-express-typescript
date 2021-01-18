d9ec8bae49f963381c2aeed0d9bfb76b55fc7a77
nicholasf <nicholasf@dawntreader-ii.lan>
2021-01-18 14:46:39 +1100

    Refactor fib controller into numbers and resource for better error handling.

---
+++ src/controllers/error.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+import { HTTPError } from "Main";
+
+const controller = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status((err as HTTPError).status)
+    res.json({ error: err })
+}
+
+export default controller;
---
+++ src/controllers/number.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import fibonacci from "../lib/fibonacci";
+
+const controller = (req: Request, res: Response, next: NextFunction) => {
+    const param = req.params['position'];
+    const position = parseInt(param);
+
+    if (position >= 0) {
+        res.locals.number = fibonacci(position) ;
+        res.locals.position = position;
+        next();
+    } else {
+        next({
+            message: "Please submit a positive number signifying a postition in the Fibonacci sequence.",
+            name: "ValidatonError",
+            status: 400
+        })
+    }
+}
+
+export default controller;
